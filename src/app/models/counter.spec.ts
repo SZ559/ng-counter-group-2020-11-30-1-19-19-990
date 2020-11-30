a304989de31256e76f194f813e2c88da06e079bc
Shishi Zhang <SZhang42@slb.com>
2020-11-30 13:28:34 +0800

    feat: should set account when create an instance

--- src/app/models/counter.spec.ts
+++ src/app/models/counter.spec.ts
@@ -4,4 +4,17 @@ describe('Counter', () => {
   it('should create an instance', () => {
     expect(new Counter()).toBeTruthy();
   });
+  it('should set account when create an instance', () => {
+    expect(new Counter().account).toBe(0);
+  });
+  it('should set account when create an instance', () => {
+    // given
+    const account = 10;
+
+    // when
+    const result = new Counter(account);
+
+    // then
+    expect(result.account).toBe(account);
+  });
 });
--- src/app/models/counter.ts
+++ src/app/models/counter.ts
@@ -1,2 +1,7 @@
 export class Counter {
+  public account: number = 0;
+  constructor(account: number = 0)
+  {
+    this.account = account;
+  }
 }
